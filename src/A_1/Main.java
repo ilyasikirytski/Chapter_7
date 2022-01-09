547fef5beb7a63807f86b3e083ca77adee4ca9c6
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-09 12:48:49 +0300

    Initial commit - A_1 - task done

---
+++ src/A_1/Main.java
@@ -0,0 +1,15 @@
+/*
+1. В каждом слове текста k-ю букву заменить заданным символом. Если k
+больше длины слова, корректировку не выполнять.
+ */
+package A_1;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hello world. My name is ilya. And i learn how to be a good dev.";
+        replaceChar(text, "o", "&@$");
+    }
+    public static void replaceChar (String text, String targetChar, String replaceTo){
+        System.out.println(text.replace(targetChar, replaceTo));
+    }
+}
