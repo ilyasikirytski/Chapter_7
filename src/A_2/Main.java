2ed977c045bbc6826decf0f86d90606f3d756a8b
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-09 13:20:09 +0300

    A_2 - task done

---
+++ src/A_2/Main.java
@@ -0,0 +1,22 @@
+/*
+2. В тексте каждую букву заменить ее порядковым номером в алфавите. При
+выводе в одной строке печатать текст с двумя пробелами между буквами,
+в следующей строке внизу под каждой буквой печатать ее номер.
+ */
+package A_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "hello world. my name is ilya. and i learn how to be a good dev.";
+        replaceCharOnAlphabetIndex(text, "e");
+    }
+
+    public static void replaceCharOnAlphabetIndex(String text, String indexOfChar) {
+        int res = 0;
+        String s = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < s.length(); i++) {
+            res = s.indexOf(indexOfChar);
+        }
+        System.out.println(text.replace(indexOfChar, Integer.toString(res + 1)));
+    }
+}
