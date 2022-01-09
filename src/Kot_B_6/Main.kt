aa3a690872188e18daae93da8c0293db1bd42089
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-09 17:14:27 +0300

    Kot_A_6 - task done

---
+++ src/Kot_B_6/Main.kt
@@ -0,0 +1,14 @@
+package Kot_B_6
+
+fun main() {
+    val text = "Do you think it is a good position? i founded a better position"
+    insertSub(text, 0, "t", "#")
+}
+
+fun insertSub(text: String, insertOrDelete: Int, insertAfter: String, charToInsert: String) {
+    if (insertOrDelete == 0) {
+        println(text.replace(insertAfter.toRegex(), insertAfter + charToInsert))
+    } else {
+        println(text.replace(insertAfter.toRegex(), ""))
+    }
+}
