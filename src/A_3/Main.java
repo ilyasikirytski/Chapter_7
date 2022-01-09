a2361cdae34f739c8b1f327575d2f6de496359c8
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-09 13:28:45 +0300

    A_3 - task done

---
+++ src/A_3/Main.java
@@ -0,0 +1,20 @@
+/*
+3. В тексте после буквы Р, если она не последняя в слове,
+ошибочно напечатана буква А вместо О. Внести исправления в текст.
+ */
+package A_3;
+
+public class Main {
+    public static void main(String[] args) {
+        String wrongText = "Do you think it is a good pasition? i founded a better pasition";
+
+        correctText(wrongText);
+    }
+
+    public static void correctText(String wrongText) {
+        if (wrongText.contains("pa")) {
+            System.out.println(wrongText.replace("pa", "po"));
+        }
+
+    }
+}
